361406a1bf8ce2aefc45291a33c8e870564e04ea
Volodymyr <allgoodsbazaaar@gmail.com>
2023-11-14 19:46:24 +0200

    update code

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import {
   StyledReviewsSection,
   StyledReviewsItem,
@@ -7,9 +8,47 @@ import {
   StyledReviewsList,
   StyledRevSecTitle,
   StyledReviewsText,
+  StyledReviewsInput,
+  StyledTextArea,
+  StyledRevThdTitle,
+  StyledReviewsForm,
+  StyledReviewsBtn,
 } from './Reviews.styled';
 
 const Reviews = () => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [comment, setComment] = useState('');
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = event => {
+    const { name, value } = event.currentTarget;
+
+    name === 'name' && setName(value);
+    name === 'phone' && setPhone(Number(value));
+    name === 'comment' && setComment(value);
+  };
+
+  const handleSubmitForm = event => {
+    event.preventDefault();
+
+    if (name === '' || phone === '' || comment === '') {
+      Notify.failure('Заповніть, будь ласка, усі поля');
+
+      return;
+    }
+
+    if (isNaN(phone)) {
+      Notify.failure('Введіть, будь ласка, правильний номер');
+
+      return;
+    }
+
+    console.log({ name, phone, comment });
+
+    setIsSubmit(true);
+  };
+
   return (
     <StyledReviewsSection>
       <StyledRevSecTitle>Відгуки наших покупців</StyledRevSecTitle>
@@ -17,26 +56,53 @@ const Reviews = () => {
         <StyledReviewsItem>
           <StyledThirdTitle>Юлія Приходько</StyledThirdTitle>
           <StyledCityText>м.Київ</StyledCityText>
-          <StyledReviewsText>
-            Дитина в захваті, дякую.
-          </StyledReviewsText>
+          <StyledReviewsText>Дитина в захваті, дякую.</StyledReviewsText>
         </StyledReviewsItem>
         <StyledReviewsItem>
           <StyledThirdTitle>Ярослав Бабик</StyledThirdTitle>
           <StyledCityText>м.Шепетівка</StyledCityText>
           <StyledReviewsText>
-            Користуюсь таким більше року. Гарний інструмент - рекомендую.
+            Придбали набір для подарунку. Гарний дерев'яний бокс.
           </StyledReviewsText>
         </StyledReviewsItem>
         <StyledReviewsItem>
-          <StyledThirdTitle>Дмитро Литвинчук</StyledThirdTitle>
+          <StyledThirdTitle>Оля Литвинчук</StyledThirdTitle>
           <StyledCityText>м.Кривий Ріг</StyledCityText>
           <StyledReviewsText>
-            Шуруповерт просто вогонь, свої функції виконує на ура, легкий та
-            потужний, рекомендую до покупки.
+            Малечі найбільше сподобались оці олійні крейди, деякі вже наполовину
+            змалювала.
           </StyledReviewsText>
         </StyledReviewsItem>
       </StyledReviewsList>
+      {!isSubmit ? (
+        <>
+          <StyledRevThdTitle>Залишити відгук</StyledRevThdTitle>
+          <StyledReviewsForm onSubmit={handleSubmitForm}>
+            <StyledReviewsInput
+              type="text"
+              name="name"
+              placeholder="Ім'я"
+              onChange={handleChange}
+            />
+            <StyledReviewsInput
+              type="tel"
+              name="phone"
+              placeholder="Номер телефону"
+              onChange={handleChange}
+            />
+            <StyledTextArea
+              name="comment"
+              cols="10"
+              rows="3"
+              placeholder="Коментар"
+              onChange={handleChange}
+            ></StyledTextArea>
+            <StyledReviewsBtn type="submit">Відправити</StyledReviewsBtn>
+          </StyledReviewsForm>
+        </>
+      ) : (
+        <StyledRevThdTitle>Дякуємо за відгук!</StyledRevThdTitle>
+      )}
     </StyledReviewsSection>
   );
 };
--- src/components/Reviews/Reviews.styled.js
+++ src/components/Reviews/Reviews.styled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { colors } from 'styles/globalStyles';
 import { StyledSection } from 'components/Description/Description.styled';
 import { StyledSecTitle } from 'components/Benefits/Benefits.styled';
+import { StyledInput } from 'components/Form/Form.styled';
 
 export const StyledReviewsSection = styled(StyledSection)`
   display: flex;
@@ -15,6 +16,7 @@ export const StyledReviewsSection = styled(StyledSection)`
 
 export const StyledRevSecTitle = styled(StyledSecTitle)`
   margin-bottom: 15px;
+
   color: ${colors.mainTextColor};
 `;
 
@@ -57,4 +59,47 @@ export const StyledReviewsText = styled.p`
   line-height: 1.2;
 
   color: ${colors.secondaryTextColor};
+`;
+
+export const StyledReviewsForm = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+`;
+
+export const StyledReviewsInput = styled(StyledInput)`
+  padding: 5px 5px 5px 10px;
+  margin-bottom: 5px;
+  width: 160px;
+  font-size: 12px;
+`;
+
+export const StyledTextArea = styled.textarea`
+  padding: 5px 5px 5px 10px;
+  margin-bottom: 5px;
+  font-size: 12px;
+  border-radius: 10px;
+  border: 2px solid ${colors.secondaryTextColor};
+  outline: none;
+
+  &:focus {
+    border: 2px solid ${colors.focusColor};
+  }
+`;
+
+export const StyledRevThdTitle = styled(StyledSecTitle)`
+  margin-bottom: 5px;
+  font-size: 20px;
+
+  color: ${colors.mainTextColor};
+`;
+
+export const StyledReviewsBtn = styled.button`
+  padding: 3px 10px;
+  width: 80px;
+  font-size: 12px;
+
+  border: none;
+  border-radius: 10px;
+  background-color: ${colors.mainTextColor};
 `;
