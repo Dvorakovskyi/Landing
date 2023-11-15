4f5ecfffaf9521130efe683f5db6f9b5504d89b1
Volodymyr <allgoodsbazaaar@gmail.com>
2023-11-15 10:05:50 +0200

    update code

--- src/components/Benefits/Benefits.jsx
+++ src/components/Benefits/Benefits.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import { StyledSection } from 'components/Description/Description.styled';
-import { StyledSecTitle, StyledItem } from './Benefits.styled';
+import benefits from '../../images/benefits/benefits.png';
+import { StyledSection, StyledDescImg } from 'components/Description/Description.styled';
 
 const Benefits = () => {
   return (
     <StyledSection>
-      <StyledSecTitle>Особливості</StyledSecTitle>
-      <ul>
-        <StyledItem>
-          Різноманітність предметів, що знаходяться в наборі;
-        </StyledItem>
-        <StyledItem>
-          Практична та міцна валізка для безпечного зберігання та переміщення;
-        </StyledItem>
-        <StyledItem>
-          Комірки набору ергономічні-групи предметів розділені на секції;
-        </StyledItem>
-        <StyledItem>Легко використовувати і зберігати;</StyledItem>
-        <StyledItem>
-          Хороша економія при покупці набору, в порівнянні з придбанням товарів
-          окремо.
-        </StyledItem>
-      </ul>
+      <StyledDescImg src={benefits} alt="Переваги" />
     </StyledSection>
   );
 };
--- src/components/CompleteSet/CompleteSet.jsx
+++ src/components/CompleteSet/CompleteSet.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import { StyledSection } from 'components/Description/Description.styled';
+import complete from '../../images/completeSet/complete.png';
+import { StyledSection, StyledDescImg } from 'components/Description/Description.styled';
 import { StyledSecTitle, StyledItem } from '../Benefits/Benefits.styled';
 
 const CompleteSet = () => {
   return (
     <StyledSection>
-      <StyledSecTitle>Комплектація</StyledSecTitle>
-      <ul>
-        <StyledItem>40 воскових пастелей</StyledItem>
-        <StyledItem>40 масляних пастелей</StyledItem>
-        <StyledItem>36 фломастерів</StyledItem>
-        <StyledItem>45 скріпок</StyledItem>
-        <StyledItem>20 великих маркерів</StyledItem>
-        <StyledItem>20 кольорових олівців</StyledItem>
-        <StyledItem>12 акварелей</StyledItem>
-        <StyledItem>1 палітра</StyledItem>
-        <StyledItem>1 кисть</StyledItem>
-        <StyledItem>1 точилка</StyledItem>
-        <StyledItem>1 клей</StyledItem>
-        <StyledItem>1 лінійка</StyledItem>
-        <StyledItem>1 гумка</StyledItem>
-        <StyledItem>1 олівець</StyledItem>
-      </ul>
+      <StyledDescImg src={complete} alt="Комплектація" />
     </StyledSection>
   );
 };
