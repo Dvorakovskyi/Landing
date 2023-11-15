898656587813504251938b78642a74eceaf9cdd2
Volodymyr <allgoodsbazaaar@gmail.com>
2023-11-15 10:06:15 +0200

    Update CompleteSet.jsx

--- src/components/CompleteSet/CompleteSet.jsx
+++ src/components/CompleteSet/CompleteSet.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import complete from '../../images/completeSet/complete.png';
 import { StyledSection, StyledDescImg } from 'components/Description/Description.styled';
-import { StyledSecTitle, StyledItem } from '../Benefits/Benefits.styled';
 
 const CompleteSet = () => {
   return (
